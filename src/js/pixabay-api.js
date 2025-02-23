30b3faf1122622ec3c6306a2f5f3d382be73a8c4
kopasovska <v.kopasovska@gmail.com>
2025-02-23 15:47:23 +0100

    Small corrections

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const myApiKey = '48965067-3962fe69a0505f7cd9cc5ea86';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://pixabay.com/',
+  baseURL: 'https://pixabay.com/api',
   params: {
     key: myApiKey,
     image_type: 'photo',
@@ -17,7 +17,5 @@ export function getImages(searchQuery) {
     q: encodeURIComponent(searchQuery),
   };
 
-  return axiosInstance
-    .get('/api', { params })
-    .then(response => response.data.hits);
+  return axiosInstance.get('', { params }).then(response => response.data.hits);
 }
